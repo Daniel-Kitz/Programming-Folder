1edffea3f36532030d13bd0b2b4fbc069f75ec8a
LordQuantumKeks <lordquantumkeks@gmail.com>
2020-05-10 12:02:21 +0200

    added stylesheet and linked to website

--- dashboard/dashboard/dashboard/dashboard/settings.py
+++ dashboard/dashboard/dashboard/dashboard/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard_app.apps.DashboardAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
